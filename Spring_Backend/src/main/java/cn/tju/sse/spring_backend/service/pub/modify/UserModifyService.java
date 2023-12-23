7242a3d73de96e980dd2d541ca257486eea88446
Faran-sward <3026418423@qq.com>
2023-12-23 17:09:40 +0800

    更改用户信息api中添加了加密密码的功能

--- Spring_Backend/src/main/java/cn/tju/sse/spring_backend/service/pub/modify/UserModifyService.java
+++ Spring_Backend/src/main/java/cn/tju/sse/spring_backend/service/pub/modify/UserModifyService.java
@@ -1,13 +1,21 @@
 package cn.tju.sse.spring_backend.service.pub.modify;
 
+import cn.tju.sse.spring_backend.dto.pub.modify.StoreModifyRequestDTO;
+import cn.tju.sse.spring_backend.dto.pub.modify.StoreModifyResponseDTO;
 import cn.tju.sse.spring_backend.dto.pub.modify.UserModifyRequestDTO;
 import cn.tju.sse.spring_backend.dto.pub.modify.UserModifyResponseDTO;
 import cn.tju.sse.spring_backend.dto.pub.modify.mapper.UserModifyRequestMapper;
 import cn.tju.sse.spring_backend.model.UsersEntity;
 import cn.tju.sse.spring_backend.repository.pub.modify.UserModifyRepository;
+import cn.tju.sse.spring_backend.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @ClassName UserModifyService
+ * @Author RaoJi
+ * @Description 修改用户信息的业务逻辑
+ */
 @Service
 public class UserModifyService {
     @Autowired
@@ -16,6 +24,12 @@ public class UserModifyService {
     private final UserModifyRequestMapper userModifyRequestMapper
             = UserModifyRequestMapper.INSTANCE;
 
+    /**
+     * @param request 前端发送来的消息
+     * @return 返回给前端的信息
+     * @see UserModifyRequestDTO
+     * @see UserModifyResponseDTO
+     */
     public UserModifyResponseDTO UserModify(UserModifyRequestDTO request){
         UserModifyResponseDTO response = new UserModifyResponseDTO();
 
@@ -35,7 +49,7 @@ public class UserModifyService {
         UsersEntity user = userModifyRequestMapper.requestToEntity(request);
         UsersEntity exists = userModifyRepository.findById(Integer.valueOf(request.getUser_ID())).orElseThrow();
         exists.setUserPhone(user.getUserPhone());
-        exists.setUserPassword(user.getUserPassword());
+        exists.setUserPassword(SecurityUtils.encodePassword(user.getUserPassword()));  // encode
         exists.setUserAddress(user.getUserAddress());
         UsersEntity update = userModifyRepository.save(exists);
 
